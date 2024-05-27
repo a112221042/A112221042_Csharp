d7c159cd279f7b3655e3982afb78d05e6dd344f8
a112221042 <a112221042@mail.shu.edu.tw>
2024-05-27 17:01:06 +0800

    2024/5/27

--- Ch7 Tutorial/Tutorial 7-2/Test Average/Test Average/Form1.Designer.cs
+++ Ch7 Tutorial/Tutorial 7-2/Test Average/Test Average/Form1.Designer.cs
@@ -39,85 +39,96 @@
             this.getScoresButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
             this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.medianscorelabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // averageScoreDescriptionLabel
             // 
             this.averageScoreDescriptionLabel.AutoSize = true;
-            this.averageScoreDescriptionLabel.Location = new System.Drawing.Point(136, 98);
+            this.averageScoreDescriptionLabel.Location = new System.Drawing.Point(204, 146);
+            this.averageScoreDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.averageScoreDescriptionLabel.Name = "averageScoreDescriptionLabel";
-            this.averageScoreDescriptionLabel.Size = new System.Drawing.Size(73, 12);
+            this.averageScoreDescriptionLabel.Size = new System.Drawing.Size(110, 18);
             this.averageScoreDescriptionLabel.TabIndex = 9;
             this.averageScoreDescriptionLabel.Text = "Average Score";
             // 
             // lowScoreDescriptionLabel
             // 
             this.lowScoreDescriptionLabel.AutoSize = true;
-            this.lowScoreDescriptionLabel.Location = new System.Drawing.Point(142, 65);
+            this.lowScoreDescriptionLabel.Location = new System.Drawing.Point(213, 98);
+            this.lowScoreDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lowScoreDescriptionLabel.Name = "lowScoreDescriptionLabel";
-            this.lowScoreDescriptionLabel.Size = new System.Drawing.Size(67, 12);
+            this.lowScoreDescriptionLabel.Size = new System.Drawing.Size(102, 18);
             this.lowScoreDescriptionLabel.TabIndex = 8;
             this.lowScoreDescriptionLabel.Text = "Lowest Score";
             // 
             // highScoreDescriptionLabel
             // 
             this.highScoreDescriptionLabel.AutoSize = true;
-            this.highScoreDescriptionLabel.Location = new System.Drawing.Point(140, 32);
+            this.highScoreDescriptionLabel.Location = new System.Drawing.Point(210, 48);
+            this.highScoreDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.highScoreDescriptionLabel.Name = "highScoreDescriptionLabel";
-            this.highScoreDescriptionLabel.Size = new System.Drawing.Size(69, 12);
+            this.highScoreDescriptionLabel.Size = new System.Drawing.Size(105, 18);
             this.highScoreDescriptionLabel.TabIndex = 7;
             this.highScoreDescriptionLabel.Text = "Highest Score";
             // 
             // testScoresListBox
             // 
             this.testScoresListBox.FormattingEnabled = true;
-            this.testScoresListBox.ItemHeight = 12;
-            this.testScoresListBox.Location = new System.Drawing.Point(12, 27);
+            this.testScoresListBox.ItemHeight = 18;
+            this.testScoresListBox.Location = new System.Drawing.Point(18, 40);
+            this.testScoresListBox.Margin = new System.Windows.Forms.Padding(4);
             this.testScoresListBox.Name = "testScoresListBox";
-            this.testScoresListBox.Size = new System.Drawing.Size(120, 88);
+            this.testScoresListBox.Size = new System.Drawing.Size(178, 130);
             this.testScoresListBox.TabIndex = 6;
             // 
             // testScoresDescriptionLabel
             // 
             this.testScoresDescriptionLabel.AutoSize = true;
-            this.testScoresDescriptionLabel.Location = new System.Drawing.Point(12, 12);
+            this.testScoresDescriptionLabel.Location = new System.Drawing.Point(18, 18);
+            this.testScoresDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.testScoresDescriptionLabel.Name = "testScoresDescriptionLabel";
-            this.testScoresDescriptionLabel.Size = new System.Drawing.Size(57, 12);
+            this.testScoresDescriptionLabel.Size = new System.Drawing.Size(89, 18);
             this.testScoresDescriptionLabel.TabIndex = 5;
             this.testScoresDescriptionLabel.Text = "Test Scores";
             // 
             // highScoreLabel
             // 
             this.highScoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.highScoreLabel.Location = new System.Drawing.Point(218, 27);
+            this.highScoreLabel.Location = new System.Drawing.Point(327, 40);
+            this.highScoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.highScoreLabel.Name = "highScoreLabel";
-            this.highScoreLabel.Size = new System.Drawing.Size(100, 21);
+            this.highScoreLabel.Size = new System.Drawing.Size(149, 30);
             this.highScoreLabel.TabIndex = 10;
             this.highScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lowScoreLabel
             // 
             this.lowScoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lowScoreLabel.Location = new System.Drawing.Point(218, 60);
+            this.lowScoreLabel.Location = new System.Drawing.Point(327, 90);
+            this.lowScoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lowScoreLabel.Name = "lowScoreLabel";
-            this.lowScoreLabel.Size = new System.Drawing.Size(100, 21);
+            this.lowScoreLabel.Size = new System.Drawing.Size(149, 30);
             this.lowScoreLabel.TabIndex = 11;
             this.lowScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // averageScoreLabel
             // 
             this.averageScoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.averageScoreLabel.Location = new System.Drawing.Point(218, 93);
+            this.averageScoreLabel.Location = new System.Drawing.Point(327, 134);
+            this.averageScoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.averageScoreLabel.Name = "averageScoreLabel";
-            this.averageScoreLabel.Size = new System.Drawing.Size(100, 21);
+            this.averageScoreLabel.Size = new System.Drawing.Size(149, 30);
             this.averageScoreLabel.TabIndex = 12;
             this.averageScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // getScoresButton
             // 
-            this.getScoresButton.Location = new System.Drawing.Point(90, 138);
+            this.getScoresButton.Location = new System.Drawing.Point(135, 284);
+            this.getScoresButton.Margin = new System.Windows.Forms.Padding(4);
             this.getScoresButton.Name = "getScoresButton";
-            this.getScoresButton.Size = new System.Drawing.Size(75, 21);
+            this.getScoresButton.Size = new System.Drawing.Size(112, 32);
             this.getScoresButton.TabIndex = 13;
             this.getScoresButton.Text = "Get Scores";
             this.getScoresButton.UseVisualStyleBackColor = true;
@@ -125,9 +136,10 @@
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(171, 138);
+            this.exitButton.Location = new System.Drawing.Point(280, 284);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(75, 21);
+            this.exitButton.Size = new System.Drawing.Size(112, 32);
             this.exitButton.TabIndex = 14;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
@@ -137,21 +149,44 @@
             // 
             this.openFile.FileName = "openFileDialog1";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 202);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Median Score";
+            // 
+            // medianscorelabel
+            // 
+            this.medianscorelabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.medianscorelabel.Location = new System.Drawing.Point(327, 196);
+            this.medianscorelabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.medianscorelabel.Name = "medianscorelabel";
+            this.medianscorelabel.Size = new System.Drawing.Size(149, 30);
+            this.medianscorelabel.TabIndex = 12;
+            this.medianscorelabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 173);
+            this.ClientSize = new System.Drawing.Size(681, 403);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.getScoresButton);
+            this.Controls.Add(this.medianscorelabel);
             this.Controls.Add(this.averageScoreLabel);
             this.Controls.Add(this.lowScoreLabel);
             this.Controls.Add(this.highScoreLabel);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.averageScoreDescriptionLabel);
             this.Controls.Add(this.lowScoreDescriptionLabel);
             this.Controls.Add(this.highScoreDescriptionLabel);
             this.Controls.Add(this.testScoresListBox);
             this.Controls.Add(this.testScoresDescriptionLabel);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Test Average";
             this.ResumeLayout(false);
@@ -172,6 +207,8 @@
         private System.Windows.Forms.Button getScoresButton;
         private System.Windows.Forms.Button exitButton;
         private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label medianscorelabel;
     }
 }
 
--- Ch7 Tutorial/Tutorial 7-2/Test Average/Test Average/Form1.cs
+++ Ch7 Tutorial/Tutorial 7-2/Test Average/Test Average/Form1.cs
@@ -13,7 +13,7 @@ namespace Test_Average
 {
     public partial class Form1 : Form
     {
-        private const int SIZE = 5;
+        private const int SIZE = 40;
         public Form1()
         {
             InitializeComponent();
@@ -23,6 +23,9 @@ namespace Test_Average
         {
             int[] scores = new int[SIZE];
             int highestScore = 0;
+            int lowestScore = 0;
+            double average = 0.0;
+            double median = 0.0;
             GetScoresFromFile(scores);
 
             //for (int i = 0; i < scores.Length; i++)
@@ -37,6 +40,29 @@ namespace Test_Average
             highestScore = Highest(scores);
             highScoreLabel.Text = highestScore.ToString();
 
+            lowestScore = Lowest(scores);
+            lowScoreLabel.Text = lowestScore.ToString();
+
+            average = Average(scores);
+            averageScoreLabel.Text = average.ToString();
+
+            
+            median = Median(scores);
+            medianscorelabel.Text = median.ToString(); 
+
+        }
+
+        private int Lowest(int[] scores)
+        {
+            int lowest = scores[0];
+            for (int i = 1; i < scores.Length; i++)
+            {
+                if (scores[i] < lowest)
+                {
+                    lowest = scores[i];
+                }
+            }
+            return lowest;
         }
 
         private int Highest(int[] scores)
@@ -52,7 +78,41 @@ namespace Test_Average
             return highest;
         }
 
-        private void GetScoresFromFile(int[] scores)
+        private double Average(int[] scores)
+        {
+            int sum = 0;
+            for (int i = 0; i<scores.Length; i++)
+            {
+                sum += scores[i]; //sum = sum + score[i]
+           
+            }
+            //MessageBox.Show("sum =" + sum);
+            return sum / scores.Length;//casting
+
+        }
+
+        private double Median(int[] scores)
+        {
+
+            Array.Sort(scores);
+
+
+            if (scores.Length % 2 != 0)
+            {
+                return scores[scores.Length / 2];
+            }
+            else
+            {
+
+                int midIndex1 = scores.Length / 2 - 1;
+                int midIndex2 = scores.Length / 2;
+                return (scores[midIndex1] + scores[midIndex2]) / 2.0;
+            }
+        }
+
+
+
+            private void GetScoresFromFile(int[] scores)
         {
             StreamReader inputFile = null;
             int index = 0;
@@ -79,5 +139,7 @@ namespace Test_Average
             // Close the form.
             this.Close();
         }
+
+        
     }
 }
