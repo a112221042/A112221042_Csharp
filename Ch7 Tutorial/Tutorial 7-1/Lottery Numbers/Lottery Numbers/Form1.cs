e13e1054c66aaebdea747317dfd513e5f47e8e5c
a112221042 <a112221042@mail.shu.edu.tw>
2024-05-27 15:34:55 +0800

    20240527_1534

---
+++ Ch7 Tutorial/Tutorial 7-1/Lottery Numbers/Lottery Numbers/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery_Numbers
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            int[] lotteryNumbers = new int[5];
+            Random rand = new Random();
+            Label[] labelsArray = { firstLabel, secondLabel, thirdLabel, fourthLabel, fifthLabel };
+
+            for (int i = 0; i < lotteryNumbers.Length; i++)
+            {
+                lotteryNumbers[i] = rand.Next(42) + 1;
+            }
+
+            //fifthLabel.Text = lotteryNumbers[0].ToString();
+            //secondLabel.Text = lotteryNumbers[1].ToString();
+
+            for (int i = 0; i < labelsArray.Length; i++)
+            {
+                labelsArray[i].Text = lotteryNumbers[i].ToString();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
---
+++ Ch7 Tutorial/Tutorial 7-2/Test Average/Test Average/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        private const int SIZE = 5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            int[] scores = new int[SIZE];
+            int highestScore = 0;
+            GetScoresFromFile(scores);
+
+            //for (int i = 0; i < scores.Length; i++)
+            //{
+            //    testScoresListBox.Items.Add(scores[i]);
+            //}
+            foreach(int value in scores)
+            {
+                testScoresListBox.Items.Add(value);
+            }
+
+            highestScore = Highest(scores);
+            highScoreLabel.Text = highestScore.ToString();
+
+        }
+
+        private int Highest(int[] scores)
+        {
+            int highest = scores[0];
+            for (int i = 1; i < scores.Length; i++)
+            {
+                if (scores[i] > highest)
+                {
+                    highest = scores[i];
+                }
+            }
+            return highest;
+        }
+
+        private void GetScoresFromFile(int[] scores)
+        {
+            StreamReader inputFile = null;
+            int index = 0;
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                inputFile = File.OpenText(openFile.FileName);
+
+                while (!inputFile.EndOfStream && index < scores.Length )
+                {
+                    scores[index] = int.Parse(inputFile.ReadLine());
+                    index++;
+                }
+            }
+            else
+            {
+                MessageBox.Show("已取消");
+            }
+            inputFile.Close();
+        }
+
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
