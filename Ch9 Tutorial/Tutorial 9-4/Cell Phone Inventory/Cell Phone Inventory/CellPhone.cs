b3a441c2cac3e648898c0e0a3adb44af5d847ff1
a112221042 <a112221042@mail.shu.edu.tw>
2024-06-03 17:53:51 +0800

    2024/6/03

---
+++ Ch9 Tutorial/Tutorial 9-4/Cell Phone Inventory/Cell Phone Inventory/CellPhone.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cell_Phone_Inventory
+{
+    class CellPhone
+    {
+        // Fields
+        private string _brand;   // The phone's brand
+        private string _model;   // The phone's model
+        private decimal _price;  // Retail price
+
+        // Constructor
+       public CellPhone()
+        {
+            _brand = "";
+            _model = "";
+            _price = 0m;
+        }
+
+
+
+        // Brand property
+        public string Brand
+        {
+            get;
+            set;
+        }
+
+
+
+        // Model property
+        public string Model
+        {
+            get;
+            set;
+        }
+
+
+
+        // Price property
+        public decimal Price
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Ch9 Tutorial/Tutorial 9-4/Cell Phone Inventory/Cell Phone Inventory/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cell_Phone_Inventory
+{
+    public partial class Form1 : Form
+    {
+        // List to hold CellPhone objects
+        List<CellPhone> phoneList = new List<CellPhone>();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The GetPhoneData method accepts a CellPhone object
+        // as an argument. It assigns the data entered by the
+        // user to the object's properties.
+       
+
+
+
+
+
+        private void addPhoneButton_Click(object sender, EventArgs e)
+        {
+            // Create a CellPhone object.
+            CellPhone thisPhone = new CellPhone();
+
+
+            // Get the phone data.
+            decimal price;
+            thisPhone.Brand = brandTextBox.Text;
+            thisPhone.Model = brandTextBox.Text;
+            if ( decimal.TryParse(priceTextBox.Text,out price))
+            {
+                thisPhone.Price = price;
+            }
+            else
+            {
+                MessageBox.Show("資料格式錯誤");
+            }
+
+
+            // Add the CellPhone object to the List.
+
+            phoneList.Add(thisPhone);
+
+            // Add an entry to the list box.
+            phoneListBox.Items.Add(thisPhone.Brand + "" + thisPhone.Model);
+
+
+
+            // Clear the TextBox controls.
+            brandTextBox.Text = "";
+            modelTextBox.Text = "";
+            priceTextBox.Text = "";
+
+
+            // Reset the focus.
+            brandTextBox.Focus();
+
+        }
+
+        private void phoneListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get the index of the selected item.
+            int index = phoneListBox.SelectedIndex;
+
+
+            // Display the selected item's price.
+            MessageBox.Show("售價:" + phoneList[index].Price).ToString("c");
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
